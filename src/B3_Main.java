0cd7e2e45c8ffc6b32611babbdb840d3b3db6eb0
noreply97 <115534973+noreply97@users.noreply.github.com>
2025-07-26 16:18:29 +0400

    Ajout projet final

--- src/B3_Main.java
+++ src/B3_Main.java
@@ -6,14 +6,18 @@ public class B3_Main {
         B3_Automaton auto3= new B3_Automaton();
         B3_Automaton auto4= new B3_Automaton();
 
-        auto1 = B3_Automaton.readFromFile("src/Automates/B3_06.txt");
+        auto1 = B3_Automaton.readFromFile("src/Automates/B3_1.7txt");
         auto1.displayAutomaton();
         auto1.whatIsIt();
-
-        auto2 = auto1.determinizeAutomaton();
+        auto2 = auto1.standardizeAutomaton();
+        auto2.displayAutomaton();
         auto3 = auto2.completeAutomaton();
         auto3.displayAutomaton();
         auto3.whatIsIt();
+        auto4 = auto3.determinizeAutomaton();
+        auto4.displayAutomaton();
+
+
 
 
     }
